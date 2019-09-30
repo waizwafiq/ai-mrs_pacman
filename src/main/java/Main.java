f790458a17bee7335c6d6265cc5386efa6e1b5c4
kok <kfkok4@yahoo.com>
2019-09-30 16:23:18 +0800

    Modify Main.java to remove exclusion Implented tree search pacman
    Signed-off-by: kok <kfkok4@yahoo.com>

---
+++ src/main/java/Main.java
@@ -0,0 +1,40 @@
+
+import examples.StarterGhostComm.Blinky;
+import examples.StarterGhostComm.Inky;
+import examples.StarterGhostComm.Pinky;
+import examples.StarterGhostComm.Sue;
+import examples.StarterPacMan.*;
+import pacman.Executor;
+import pacman.controllers.IndividualGhostController;
+import pacman.controllers.MASController;
+import pacman.game.Constants.*;
+import pacman.game.internal.POType;
+
+import java.util.EnumMap;
+
+
+/**
+ * Created by pwillic on 06/05/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Executor executor = new Executor.Builder()
+                .setVisual(true)
+                .setTickLimit(4000)
+                .setScaleFactor(3) // Increase game visual size
+                .setPOType(POType.RADIUS) // pacman sense objects around it in a radius wide fashion instead of straight line sights
+                .setSightLimit(5000) // The sight radius limit, set to maximum 
+                .build();
+
+        EnumMap<GHOST, IndividualGhostController> controllers = new EnumMap<>(GHOST.class);
+
+        controllers.put(GHOST.INKY, new Inky());
+        controllers.put(GHOST.BLINKY, new Blinky());
+        controllers.put(GHOST.PINKY, new Pinky());
+        controllers.put(GHOST.SUE, new Sue());
+
+        executor.runGameTimed(new TreeSearchPacMan(), new MASController(controllers));
+    }
+}
